d5b9d0858a6dd67abdb24c82d92da1110f4ef925
vikram1503 <vikram15031989@gmail.com>
2024-07-18 10:40:45 +0530

    practice

--- 05-eks/main.tf
+++ 05-eks/main.tf
@@ -60,6 +60,7 @@ module "eks" {
       iam_role_additional_policies = {
         AmazonEBSCSIDriverPolicy          = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
         AmazonElasticFileSystemFullAccess = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
+        ElasticLoadBalancingFullAccess = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
       }
       # EKS takes AWS Linux 2 as it's OS to the nodes
       key_name = aws_key_pair.eks.key_name
